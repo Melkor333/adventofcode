b23a30ee2ebfef3996092c9e753836933908b18d
Samuel Hierholzer <samuel.hierholzer@adfinis.com>
2022-12-03 18:39:06 +0100

    Now with functions and a bit more reasonable looking :)

--- 03/getitempriorities.c
+++ 03/getitempriorities.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
 #define EOL '\n'
+// A - 38 = 26, a - 96 = 1
+#define ToPriority(num) ((num) - ((num) < 'a' ? 38 : 96))
+int findChar(char *index, int len, char needle);
+int findInSecondHalf(char *index, int len);
 
 int main(int argc, char *argv[]) {
   char const *const filename = argv[1];
   char line[200];
-  char *bag, *bag2, *count2;
   int c, count, sum;
 
-  printf("%i %i\n", (int) 'a', (int)'A');
   FILE *file = fopen(filename, "r");
 
   for (count = 0; ((c = fgetc(file)) != EOF) && count < 200; count++) {
     if (c != EOL)
       line[count] = c;
     else {
-      bag = line;
-      bag2 = line + (count / 2);
-
-      for (; bag < bag2; bag++) {
-        count2 = bag2;
-        for (count2 = bag2;
-             (*count2 != *bag) && (count2 - line < (count-1));
-             count2++)
-          ;
-        if (*count2 == *bag) {
-          sum += *bag - (*bag < 'a' ? 38 : 96); // A - 39 = 26, a - 96 = 1
-          printf("%i %i %c\n", sum, (*bag < 'a' ? 38 : 96), *bag);
-          printf("%i %li %li\n\n", count, count2 - line, bag2 - line);
-          break;
-        }
+      c = findInSecondHalf(line, count);
+      if (c > -1)
+        sum += ToPriority(c);
+      else {
+        printf("YO Not found in second part!");
+        return 1;
       }
       count = -1;
-    }
+      c = *line;
+    };
   };
   if (count == 200) {
     printf("Count went over 200 :(\n");
     return 1;
   }
   printf("%i\n", sum);
+}
+
+int findInSecondHalf(char *index, int len) {
+  int i, c;
+  int split = len/2;
+  for (i=0; i < split; i++) {
+    if ((c = findChar(index+split, split, index[i])) > -1) {
+      printf("%i\n", c);
+      return (int) index[c + split];
+    };
+  };
+  return -1; // should never be reached
+}
+
+int findChar(char *index, int len, char needle) {
+  int c;
+  for (c = 0; c < len; c++) {
+    if (index[c] == needle)
+      return c;
+  }
+  return -1;
 }
