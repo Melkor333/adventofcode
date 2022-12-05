7946facb76912f61b9664983ff2e23ad29057955
Samuel Hierholzer <samuel.hierholzer@adfinis.com>
2022-12-05 07:53:53 +0100

    Day 3, challenge 2

--- 03/getitempriorities.c
+++ 03/getitempriorities.c
@@ -2,22 +2,38 @@
 #define EOL '\n'
 // A - 38 = 26, a - 96 = 1
 #define ToPriority(num) ((num) - ((num) < 'a' ? 38 : 96))
+struct line {
+  char string[200];
+  int len;
+};
 int findChar(char *index, int len, char needle);
 int findInSecondHalf(char *index, int len);
 int ChallengeOne(char *line, int count, int *sum);
+int ChallengeTwo(struct line group[]);
 
 int main(int argc, char *argv[]) {
   char const *const filename = argv[1];
-  char line[200];
-  int c, count, sum;
+  struct line group[3];
+  int c, count, sum, groupC;
+  groupC = sum = 0;
 
   FILE *file = fopen(filename, "r");
 
-  for (count = 0; ((c = fgetc(file)) != EOF) && count < 200; count++) {
+  for (count = 0; ((c = fgetc(file)) != EOF) && count < 199; count++) {
     if (c != EOL)
-      line[count] = c;
+      group[groupC].string[count] = c;
     else {
-      ChallengeOne(line, count, &sum);
+      // ChallengeOne(line, count, &sum);
+      // count = -1;
+
+      group[groupC].len = count;
+      group[groupC].string[count+1] = '\0';
+      if (groupC < 2)
+        groupC++;
+      else {
+        sum += ChallengeTwo(group);
+        groupC = 0;
+      }
       count = -1;
     };
   };
@@ -26,6 +42,29 @@ int main(int argc, char *argv[]) {
     return 1;
   }
   printf("%i\n", sum);
+}
+
+int ChallengeTwo(struct line group[]) {
+  int count, groupC, foundC, c;
+  char needle;
+  printf("%s, %i\n", group[0].string, group[0].len);
+  for (count = 0; count < group[0].len; count++) {
+    foundC = 0;
+    needle = group[0].string[count];
+    printf("%c ", needle);
+    for (groupC = 1; groupC < 3; groupC++) {
+      printf("%s, %i\n", group[groupC].string, group[groupC].len);
+      if ((c = findChar(group[groupC].string, group[groupC].len, needle)) > -1) {
+        printf("found %c in %s\n", needle, group[groupC].string);
+        if (foundC == 1)
+          return ToPriority(needle);
+        else
+          foundC++;
+      }
+    }
+  }
+  printf("fugg: %c\n", c);
+  return -1;
 }
 
 int ChallengeOne(char *line, int count, int *sum) {
