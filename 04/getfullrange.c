a35a8f867c6682da2a1cafb1a4c0ffac4c244635
Samuel Hierholzer <samuel.hierholzer@adfinis.com>
2022-12-05 08:33:05 +0100

    Day4, chall2

--- 04/getfullrange.c
+++ 04/getfullrange.c
@@ -10,16 +10,23 @@ int main(int argc, char *argv[]) {
 
 
   while ((c = fgetc(file)) != EOF) {
+    printf("%c", c);
     if (c >= '0' && c <= '9') {
       numbers[currentNumber] = (numbers[currentNumber]*10) + (c - '0'); // poor atoi
     }
     else if (c == ',' || c == '-')
       currentNumber++;
     else {
+      printf("\n");
+      //day1
+      //if ((numbers[0] <= numbers[2] && numbers[1] >= numbers[3]) ||
+      //    (numbers[2] <= numbers[0] && numbers[3] >= numbers[1])) {
       if ((numbers[0] <= numbers[2] && numbers[1] >= numbers[3]) ||
-          (numbers[2] <= numbers[0] && numbers[3] >= numbers[1])) {
+          (numbers[2] <= numbers[0] && numbers[3] >= numbers[1]) || // complete overlap until here
+          (numbers[1] >= numbers[3] && numbers[0] <= numbers[3]) || // partial overlap
+          (numbers[0] <= numbers[2] && numbers[1] >= numbers[2])) {
         sum++;
-        printf("overlap!");
+        printf("overlap!\n");
       }
       currentNumber = 0;
       numbers[0]= numbers[1]= numbers[2]= numbers[3] = 0;
