afadbed32d127c3e19a896fc3157621b194c3031
Samuel Hierholzer <samuel.hierholzer@adfinis.com>
2023-01-26 17:38:20 +0100

    08: Generalize

--- 08/main.c
+++ 08/main.c
@@ -1,15 +1,168 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#define MAX_ROW 500
-#define MAX_COL 500
-#define MAX_VISIBLE_TREES (MAX_ROW+MAX_COL)*2
+#define MAX_X 500
+#define MAX_Y 500
+#define MAX_VISIBLE_TREES (MAX_X*MAX_Y)
 
-int find_tree(int trees[], int num, int len) {
-  for (int i = 0; i<len; i++)
-    if (num == trees[i])
-      return 1;
-  return 0;
+#define LINE_FINISHED 1
+#define LEFT 2
+#define RIGHT 4
+#define DOWN 8
+#define UP 16
+
+typedef struct FOREST {
+  int num_x;
+  int num_y;
+  int num_visible_trees;
+  char *map[MAX_X];
+  int visible_trees[MAX_VISIBLE_TREES];
+} FOREST;
+
+typedef struct TREE {
+  int x;
+  int y;
+} TREE;
+
+int tree_size(FOREST *forest, TREE *tree) {
+  return forest->map[tree->x][tree->y] - '0';
+}
+
+void tree_copy(TREE *src, TREE *dst) {
+  dst->x = src->x;
+  dst->y = src->y;
+}
+
+int add_row(FOREST *forest, char *line) {
+    forest->map[forest->num_x++] = strdup(line);
+    if (forest->num_y == 0)
+      forest->num_y = strlen(line);
+    if (forest->num_x >= MAX_X) {
+      puts("Too many rows!");
+      exit(1);
+    }
+    return forest->num_x;
+}
+
+int get_next(FOREST *forest, TREE *tree, int mode) {
+  if (mode == 0)
+    mode = LEFT;
+  else
+    mode = mode & ~LINE_FINISHED;
+
+  // go from left down
+  if (mode & LEFT) {
+    // init
+    if (tree->x == -1) {
+      tree->x = 0;
+      tree->y = 0;
+      return mode | LINE_FINISHED;
+    }
+    // go right
+    if (++(tree->y) < forest->num_y) {
+      return mode;
+    // go one down and left
+    } else if (++(tree->x) < forest->num_x) {
+      tree->y = 0;
+      return mode | LINE_FINISHED;
+    }
+    mode = RIGHT;
+    tree->x = -1;
+  }
+  // go from right down
+  if (mode & RIGHT) {
+    // init
+    if (tree->x == -1) {
+      tree->x = 0;
+      tree->y = forest->num_y - 1;
+      return mode | LINE_FINISHED;
+    }
+    // go left
+    if (--(tree->y) >= 0) {
+      return mode;
+    // go down and right
+    } else if (++(tree->x) < forest->num_x) {
+      tree->y = forest->num_y - 1;
+      return mode | LINE_FINISHED;
+    }
+    mode = DOWN;
+    tree->x = -1;
+  }
+  // go top right
+  if (mode & DOWN) {
+    //init
+    if (tree->x == -1) {
+      tree->x = 0;
+      tree->y = 0;
+      return mode | LINE_FINISHED;
+    }
+    // go one down
+    if (++(tree->x) < forest->num_x) {
+      return mode;
+    // go one left and up to 0
+    } else if (++(tree->y) < forest->num_y) {
+      tree->x = 0;
+      return mode | LINE_FINISHED;
+    }
+    mode = UP;
+    tree->x = -1;
+  }
+  // go bot right
+  if (mode & UP) {
+    //init
+    if (tree->x == -1) {
+      tree->x = forest->num_x - 1;
+      tree->y = 0;
+      return mode | LINE_FINISHED;
+    }
+    // go one down
+    if (--(tree->x) >= 0) {
+      return mode;
+    // go one right and fully down
+    } else if (++(tree->y) < forest->num_y) {
+      tree->x = forest->num_x - 1;
+      return mode | LINE_FINISHED;
+    }
+    tree->x = -1;
+    return -1;
+  }
+  puts("something went wrong. no mode applies");
+  exit(1);
+}
+
+// add a visible tree to a forest
+int find_visible_tree(FOREST *forest, TREE *tree, int add) {
+  // merge x and y into one number: X0Y the x=1 is so that x may be 0
+  int coord = (tree->x+1)*1000 + tree->y;
+  for (int i = 0; i < forest->num_visible_trees; i++) {
+    if (coord == forest->visible_trees[i]) {
+      return 0;
+    }
+  }
+  if (add == 1)
+    forest->visible_trees[(forest->num_visible_trees)++] = coord;
+  if (forest->num_visible_trees >= MAX_VISIBLE_TREES) {
+    printf("Too many visible trees!");
+    exit(1);
+  }
+  return 1;
+}
+
+void find_visible_trees(FOREST *forest) {
+  TREE *tallest_tree;
+  TREE *tree;
+  int i;
+  tree->x = -1;
+  tree->y = -1;
+
+  int max = 0;
+  int mode = 0;
+  while ((mode = get_next(forest, tree, mode)) > 0) {
+    if (mode & LINE_FINISHED || tree_size(forest, tree) > max) {
+      find_visible_tree(forest, tree, 1); // almost forgot, EVERY VISIBLE TREE. Not just the tallest one!
+      max = tree_size(forest, tree);
+    }
+  }
 }
 
 int main(int argc, const char *argv[]) {
@@ -17,46 +170,38 @@ int main(int argc, const char *argv[]) {
     exit(1);
   const char *filename = argv[1];
   FILE *file = fopen(filename, "r");
-
-  char *tree_map[MAX_ROW];
-  char line[MAX_COL];
-  int num_rows = 0;
-  int num_col = 0;
+  FOREST forest;
+  forest.num_x = 0;
+  forest.num_y = 0;
+  forest.num_visible_trees = 0;
+  char line[MAX_Y];
   int col = 0;
-  int visible_trees[MAX_VISIBLE_TREES];
-  int num_visible_trees = 0;
 
-  while (fgets(line, MAX_COL-1, file) != NULL) {
-    if (num_rows >= MAX_ROW) {
-      printf("fuckyducki too many Rows!");
-      exit(1);
-    }
-    tree_map[num_rows] = strdup(line);
-    num_rows++;
-  }
-  // TODO: Make this a function which takes a function to calculate next x/y!
-  // See from the left
-  for (int x; x < num_rows; x++) {
-    int max_y = 0;
-    int max = 0;
-    int tree = 0;
-    for (int y = 0; y < MAX_COL; y++) {
-      if (tree_map[x][y] == '\n' || tree_map[x][y] == '\0')
-        break;
-      int cur_tree = tree_map[x][y] - '0';
-      if (cur_tree > max) {
-        max = cur_tree;
-        max_y = y;
-      }
-    }
-    if (!(find_tree(visible_trees, (x+1)*1000 + max_y, num_visible_trees))) {
-      visible_trees[num_visible_trees++] = (x+1)*1000 + max_y;
-    }
+  while (fgets(line, MAX_Y-1, file) != NULL) {
+    add_row(&forest, line);
   }
 
-  puts("Visible Trees from left:");
-  for (int i = 0; i<num_visible_trees; i++)
-    printf("%i\n", visible_trees[i]);
+  for (int i = 0; i<forest.num_visible_trees-1; i++)
+    printf("%i\n", forest.visible_trees[i]);
 
-  printf("Number of trees is: %i", num_visible_trees);
+  find_visible_trees(&forest);
+
+  // create forest map
+  int secondcount = 0;
+  for (int x = 0; x < forest.num_x; x++) {
+    for (int y = 0; y < forest.num_y -1; y++) {
+      TREE t;
+      t.x = x;
+      t.y = y;
+      if(find_visible_tree(&forest, &t, 0) == 0) {
+        printf("\033[0;31m%c\033[0m", forest.map[x][y]);
+        secondcount++;
+      } else
+        printf("%c", forest.map[x][y]);
+    }
+    printf("\n");
+  }
+  printf("Number of trees is: %i\n", forest.num_visible_trees);
+  printf("Map has size: %i %i\n", forest.num_x, forest.num_y);
+  printf("Second count is: %i\n", secondcount);
 }
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 My [advent of code](https://adventofcode.com/2022) 2022 solutions in some poor C :)
+
+# Specialities
+## Pre Challenge 7
+
+Reading a file, writing `unity` tests (copied the tests folder from a [exercism](exercism.org/) project.
+
+## Challenge 7
+
+Trying to use `#define`
+
+## Challenge 8
+
+No more tests. ~~Write function which takes another function as argument~~ Print out a nice map :)
+And the first one with seemingly proper `typedefs`.
