f708a94a8fb9e2ff6f2b49ff8439a8d4b82499ca
Samuel Hierholzer <samuel.hierholzer@adfinis.com>
2023-02-07 16:19:18 +0100

    Some more stuff but nothing that gets chal2 done

--- 08/main.c
+++ 08/main.c
@@ -4,12 +4,15 @@
 #define MAX_X 500
 #define MAX_Y 500
 #define MAX_VISIBLE_TREES (MAX_X*MAX_Y)
+#define COORD(f,x,y) (x)*((f)->num_y)+(y)
 
 #define LINE_FINISHED 1
 #define LEFT 2
 #define RIGHT 4
 #define DOWN 8
 #define UP 16
+#define OVERRIDE 32
+#define HOUSE 64
 
 typedef struct FOREST {
   int num_x;
@@ -17,6 +20,8 @@ typedef struct FOREST {
   int num_visible_trees;
   char *map[MAX_X];
   int visible_trees[MAX_VISIBLE_TREES];
+  int house_visible_trees[MAX_VISIBLE_TREES];
+  int tree_scenic[MAX_VISIBLE_TREES];
 } FOREST;
 
 typedef struct TREE {
@@ -34,9 +39,9 @@ void tree_copy(TREE *src, TREE *dst) {
 }
 
 int add_row(FOREST *forest, char *line) {
-    forest->map[forest->num_x++] = strdup(line);
     if (forest->num_y == 0)
       forest->num_y = strlen(line);
+    forest->map[forest->num_x++] = strdup(line);
     if (forest->num_x >= MAX_X) {
       puts("Too many rows!");
       exit(1);
@@ -44,11 +49,14 @@ int add_row(FOREST *forest, char *line) {
     return forest->num_x;
 }
 
-int get_next(FOREST *forest, TREE *tree, int mode) {
-  if (mode == 0)
-    mode = LEFT;
-  else
-    mode = mode & ~LINE_FINISHED;
+int next_tree(FOREST *forest, TREE *tree, long mode) {
+  //printf("%i", mode);
+  if (mode == 0) {
+    mode = LEFT | OVERRIDE; // Override on first run
+    tree->x = -1;
+  } else
+    mode = mode & (~LINE_FINISHED);
+  //printf("get next after %i %i\n", tree->x, tree->y);
 
   // go from left down
   if (mode & LEFT) {
@@ -131,38 +139,89 @@ int get_next(FOREST *forest, TREE *tree, int mode) {
 }
 
 // add a visible tree to a forest
-int find_visible_tree(FOREST *forest, TREE *tree, int add) {
+void set_tree_visibility(FOREST *forest, TREE *tree, int visible, long mode) {
   // merge x and y into one number: X0Y the x=1 is so that x may be 0
-  int coord = (tree->x+1)*1000 + tree->y;
-  for (int i = 0; i < forest->num_visible_trees; i++) {
-    if (coord == forest->visible_trees[i]) {
-      return 0;
-    }
-  }
-  if (add == 1)
-    forest->visible_trees[(forest->num_visible_trees)++] = coord;
-  if (forest->num_visible_trees >= MAX_VISIBLE_TREES) {
-    printf("Too many visible trees!");
-    exit(1);
-  }
-  return 1;
+  //int coord = (tree->x+1)*1000 + tree->y;
+  int coord = COORD(forest,tree->x,tree->y);
+  //printf("%i * %i + %i = %i\n", tree->x,forest->num_y,tree->y, coord);
+  forest->house_visible_trees[coord] = 0;
+  if (mode & OVERRIDE)
+    forest->visible_trees[coord] = visible;
+  else
+    forest->visible_trees[coord] |= visible;
+}
+
+int tree_visibility(FOREST *forest, TREE *tree, long mode) {
+  // merge x and y into one number: X0Y the x=1 is so that x may be 0
+  //int coord = (tree->x+1)*1000 + tree->y;
+  int coord = COORD(forest,tree->x,tree->y);
+  if (mode & HOUSE)
+    return forest->house_visible_trees[coord];
+  else
+    return forest->visible_trees[coord];
 }
 
 void find_visible_trees(FOREST *forest) {
   TREE *tallest_tree;
   TREE *tree;
-  int i;
   tree->x = -1;
   tree->y = -1;
 
-  int max = 0;
-  int mode = 0;
-  while ((mode = get_next(forest, tree, mode)) > 0) {
+  int max = -1;
+  long mode = 0;
+  while ((mode = next_tree(forest, tree, mode)) > 0) {
     if (mode & LINE_FINISHED || tree_size(forest, tree) > max) {
-      find_visible_tree(forest, tree, 1); // almost forgot, EVERY VISIBLE TREE. Not just the tallest one!
+      set_tree_visibility(forest, tree, 1, mode);
       max = tree_size(forest, tree);
-    }
+    } else
+      set_tree_visibility(forest, tree, 0, mode);
   }
+}
+
+int house_find_visible_trees(FOREST *forest, TREE *tree, int add_to_map) {
+  TREE tree_house;
+  int distance = 0;
+  int full_dist = 1;
+  tree_house.x = tree->x;
+  tree_house.y = tree->y;
+
+  int size = 0;
+  long mode = LEFT;
+  long last_mode = LEFT;
+  int looped = 0;
+  while ((mode = next_tree(forest, tree, mode)) > 0) {
+    if (((mode & LINE_FINISHED) == 0) && tree_size(forest, tree) >= size) {
+      size = tree_size(forest, tree);
+      distance++;
+      if (add_to_map == 1)
+        forest->house_visible_trees[COORD(forest,tree->x,tree->y)] = 1;
+      continue;
+    }
+    tree->x = tree_house.x;
+    tree->y = tree_house.y;
+    if (add_to_map == 1)
+      printf("calc %i * %i", distance, full_dist);
+    if (distance > 0)
+      full_dist *= distance;
+    distance = 0;
+    size = 0;
+    if (mode & last_mode) {
+      if (looped == 1)
+        goto finished;
+      if (mode & LEFT)
+        last_mode = mode = RIGHT;
+      else if (mode & RIGHT)
+        last_mode = mode = DOWN;
+      else if (mode & DOWN) {
+        last_mode = mode = UP;
+        looped = 1;
+      } else if (mode & UP)
+        goto finished;
+    } else
+      last_mode = mode & (LEFT | RIGHT | UP | DOWN);
+  }
+ finished:
+  return full_dist;
 }
 
 int main(int argc, const char *argv[]) {
@@ -181,27 +240,56 @@ int main(int argc, const char *argv[]) {
     add_row(&forest, line);
   }
 
-  for (int i = 0; i<forest.num_visible_trees-1; i++)
-    printf("%i\n", forest.visible_trees[i]);
+  //for (int i = 0; i<forest.num_visible_trees-1; i++)
+  //  printf("%i\n", forest.visible_trees[i]);
 
   find_visible_trees(&forest);
 
   // create forest map
   int secondcount = 0;
+  int houses_visible_trees[MAX_VISIBLE_TREES];
+  TREE tree;
+  for (tree.x = 0; tree.x < forest.num_x; (tree.x)++) {
+    for (tree.y = 0; tree.y < forest.num_y -1; (tree.y)++) {
+      if(tree_visibility(&forest, &tree, 0))
+        secondcount++;
+      int coord = COORD(&forest,tree.x,tree.y);
+      forest.house_visible_trees[COORD(&forest,tree.x,tree.y)] = 0;
+      houses_visible_trees[coord] = house_find_visible_trees(&forest, &tree, 0);
+    }
+  }
+  int max_scenic = 0;
+  TREE best_house;
+  for (int x = 0; x < forest.num_x; x++) {
+    for (int y = 0; y < forest.num_y -1; y++) {
+      int coord = x*forest.num_y+y;
+      if (max_scenic < houses_visible_trees[coord]) {
+        max_scenic = houses_visible_trees[coord];
+        best_house.x = x;
+        best_house.y = y;
+      }
+    }
+  }
+  house_find_visible_trees(&forest, &best_house, 1);
   for (int x = 0; x < forest.num_x; x++) {
     for (int y = 0; y < forest.num_y -1; y++) {
       TREE t;
       t.x = x;
       t.y = y;
-      if(find_visible_tree(&forest, &t, 0) == 0) {
+      if (forest.house_visible_trees[COORD(&forest,x,y)] == 1)
+        printf("\033[0;93m%c\033[0m", forest.map[x][y]);
+      else if(tree_visibility(&forest, &t, 0)) {
         printf("\033[0;31m%c\033[0m", forest.map[x][y]);
-        secondcount++;
       } else
         printf("%c", forest.map[x][y]);
+        //printf("%i", tree_visibility(&forest, &t));
     }
     printf("\n");
   }
+
   printf("Number of trees is: %i\n", forest.num_visible_trees);
   printf("Map has size: %i %i\n", forest.num_x, forest.num_y);
+  forest.num_visible_trees = secondcount;
   printf("Second count is: %i\n", secondcount);
+  printf("Best house coord is %i,%i with a scenic count of %i\n", best_house.x, best_house.y, max_scenic);
 }
